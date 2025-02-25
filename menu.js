5580d848ba3a31c8a3ea5a9eaf162756e9505128
Zusita <118792001+Zozuke@users.noreply.github.com>
2025-02-25 16:08:26 -0600

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,6 @@
+fetch('menu.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('menu-container').innerHTML = data;
+  })
+  .catch(error => console.error('Error al cargar el menú:', error));
