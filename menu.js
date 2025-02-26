7da8fc2bcb82b7f861550f7b31479da7b2e4c0c6
Zusita <118792001+Zozuke@users.noreply.github.com>
2025-02-25 22:08:05 -0600

    Update menu.js

--- menu.js
+++ menu.js
@@ -3,6 +3,11 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.text())
         .then(data => {
             document.getElementById('menu-container').innerHTML = data;
+            
+            // Reactivar eventos de Bootstrap después de insertar el menú
+            let scripts = document.createElement('script');
+            scripts.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js";
+            document.body.appendChild(scripts);
         })
         .catch(error => console.error('Error al cargar el menú:', error));
 });
