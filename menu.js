a3994f650d47c5a19ec13b5732ab227b3085150d
Zusita <118792001+Zozuke@users.noreply.github.com>
2025-02-25 21:29:19 -0600

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,6 +1,8 @@
-fetch('menu.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('menu-container').innerHTML = data;
-  })
-  .catch(error => console.error('Error al cargar el menú:', error));
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('menu.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('menu-container').innerHTML = data;
+        })
+        .catch(error => console.error('Error al cargar el menú:', error));
+});
